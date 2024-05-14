48c8955a1f11deed3d93be05d40471fb727385fa
albertogomcas <albertogomcas@gmail.com>
2024-05-14 12:11:42 +0200

    Async

---
+++ display.py
@@ -0,0 +1,38 @@
+from machine import SoftI2C, Pin
+import ssd1306
+import asyncio
+
+class Display:
+    def __init__(self, sda, scl, width=128, height=32):
+        i2c = SoftI2C(sda=Pin(sda), scl=Pin(scl))
+        self._display = ssd1306.SSD1306_I2C(width, height, i2c)
+        self._display.text("Starting up", 0, 0, 1)
+        self._display.show()
+
+        self._width = width
+        self._height = height
+
+        self._top = ""
+        self._middle = ""
+        self._bottom = ""
+
+    def top(self, text):
+        self._top = text
+
+    def middle(self, text):
+        self._middle = text
+
+    def bottom(self, text):
+        self._bottom = text
+
+    def refresh(self):
+        self._display.fill(0)
+        self._display.text(self._top, 0, 0, 1)
+        self._display.text(self._middle, 0, 10, 1)
+        self._display.text(self._bottom, 0, 25, 1)
+        self._display.show()
+
+    async def auto_refresh(self):
+        while True:
+            self.refresh()
+            await asyncio.sleep(0.1)
--- main.py
+++ main.py
@@ -1,76 +1,55 @@
-import time
-import utime
-from machine import Pin, PWM, ADC, SoftI2C
+import os
+import sys
+from machine import PWM, ADC, Pin
 import ssd1306
 from secrets import Wifi
 import asyncio
 from time import sleep
+from display import Display
 
 min_pulse = 1000000  # ns
 max_pulse = 2000000  # ns
 
-i2c = SoftI2C(sda=Pin(21), scl=Pin(22))
-display = ssd1306.SSD1306_I2C(128, 32, i2c)
-display.text("Starting up", 0, 0, 1)
-display.show()
-time.sleep(1)
-
+display = Display(sda=21, scl=22)
 
 def dprint(*args):
     print(*args)
     global display
-    display.fill(0)
-    display.show()
     txt = " ".join(args)
-    lines = txt.split("\n")
-    for index, line in enumerate(lines):
-        display.text(line, 0, index*9, 1)
-    display.show()
+    display.middle(txt)
 
+async def connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        display.bottom('Wifi...')
+        wlan.connect(Wifi.ssid, Wifi.password)
+        while not wlan.isconnected():
+            await asyncio.sleep(0.1)
 
-# def do_connect():
-#     import network
-#     wlan = network.WLAN(network.STA_IF)
-#     wlan.active(True)
-#     if not wlan.isconnected():
-#         print('connecting to network...')
-#         wlan.connect(Wifi.ssid, Wifi.password)
-#         while not wlan.isconnected():
-#             pass
-#     print('network config:', wlan.ifconfig())
+    display.bottom(f"Wifi {wlan.ifconfig()[0]}")
+
 
 
 def min_speed(esc):
-    dprint("set min\nspeed")
+    dprint("Low speed")
     esc.duty_ns(min_pulse)
 
 
 def max_speed(esc):
-    dprint("set max\nspeed")
+    dprint("MAX speed")
     esc.duty_ns(max_pulse)
 
 def default_speed(esc):
-    dprint("set default")
+    dprint("High speed")
     esc.duty_ns(min_pulse + (max_pulse - min_pulse) // 30)
 
-def sweep(esc):
-    dprint("sweep")
-    span = max_pulse - min_pulse
-    steps = 20
 
-    for i in range(steps):
-        duty = min_pulse + (i * span) // steps
-        dprint(f"step {i} Duty {duty}ns ({(duty - min_pulse) / span * 100}%)")
-        esc.duty_ns(duty)
-        sleep(0.5)
-
-    min_speed(esc)
-
-
-def read_button(bt):
+async def read_button(bt):
     if bt.value():
         for i in range(10):
-            sleep(0.02)
+            await  asyncio.sleep(0.02)
             if bt.value():
                 continue
             else:
@@ -80,44 +59,52 @@ def read_button(bt):
     return False
 
 
-def main():
-    # do_connect()
+async def main():
+    c = asyncio.create_task(connect())
+    drefresh = asyncio.create_task(display.auto_refresh())
 
-    calibrate = False
-
-    min_button = Pin(32, Pin.PULL_DOWN)
-    max_button = Pin(12, Pin.PULL_DOWN)
-    esc_pin = Pin(33, Pin.OUT)
-    poti = ADC(0)
-
-    esc = PWM(esc_pin, freq=50)
-
-    if calibrate:
-        max_speed(esc)
-        dprint("Turn on the ESC now")
-        dprint("Press min button when the max is calibrated")
-        while not read_button(min_button):
-            sleep(0.1)
-        dprint("Turn off the ESC")
+    try:
         calibrate = False
 
-    min_speed(esc)
+        min_button = Pin(32, Pin.IN, Pin.PULL_DOWN)
+        max_button = Pin(12, Pin.IN, Pin.PULL_DOWN)
+        esc_pin = Pin(33, Pin.OUT)
+        poti = ADC(0)
 
-    while True:
-        utime.sleep_ms(200)
-        min_pressed = read_button(min_button)
-        max_pressed = read_button(max_button)
+        esc = PWM(esc_pin, freq=50)
 
-        if min_pressed and not max_pressed:
-            min_speed(esc)
+        if calibrate:
+            max_speed(esc)
+            dprint("Turn on the ESC now")
+            dprint("Press min button when the max is calibrated")
+            while not await read_button(min_button):
+                sleep(0.1)
+            dprint("Turn off the ESC")
+            calibrate = False
 
-        if max_pressed and not min_pressed:
-            default_speed(esc)
+        min_speed(esc)
 
-        if min_pressed and max_pressed:
-            sweep(esc)
+        a = 1 / 0
 
+        while True:
+            await asyncio.sleep(0.1)
+            min_pressed = await read_button(min_button)
+            max_pressed = await read_button(max_button)
+
+            if min_pressed and not max_pressed:
+                min_speed(esc)
+
+            if max_pressed and not min_pressed:
+                default_speed(esc)
+
+
+
+    except Exception as e:
+        #fname = exc_tb.tb_frame.f_code.co_filename
+        drefresh.cancel()
+        display.top(f"Exception")
+        display.middle(f"{e}")
+        display.refresh()
 
 if __name__ == '__main__':
-    dprint("Hi")
-    main()
+    asyncio.run(main())
