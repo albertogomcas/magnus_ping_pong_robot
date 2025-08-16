a8ec93445ac91deb29fb49a0361c593bc9bfedd9
albertogomcas <albertogomcas@gmail.com>
2025-08-16 17:10:02 +0200

    Update ujrpc

--- esp_app/ujrpc.py
+++ esp_app/ujrpc.py
@@ -66,29 +66,32 @@ class JRPCService:
             _r.update({"error": JRPC2_ERRS.INVLD_PRM})
             return
         
+        # `kwargs` is an extension to pass both positional and keyword arguments
+        # this is not defined in JRPC2.0 Spec,
+        _parms = data.get("kwargs")
+        if _parms is not None:
+            kwargs = _parms
+        
         if self.debug:
-            print("params:", _parms)
+            print("method:", mtd.__name__, "params:", _parms)
 
         _r["id"] = mtd_id
         return {"method":mtd, "id":mtd_id, "args":args, "kwargs":kwargs,
                 "_self": self.bind_self if self.bind_self else self}
         
     def _hndl_err(self, ex, ctx, _r):
+        if self.debug:
+            print("RPC Handling Error", ex.args, type(ex))
         if isinstance(ex,JRPCException):
-            if self.debug:
-                print("RPC.CUSTM_ERR", ex)
-            _r.update({"error": {"code": ex, "message": ex.message, "data": ex.data}})
+            _r.update({"error": {"code": ex.code, "message": ex.message, "data": ex.data}})
         elif isinstance(ex,TypeError):
-            if self.debug:
-                print("RPC.INVLD_PRM", ex)
-            else:
+            if not self.debug:
                 _r["id"] = None
             _r.update({"error": JRPC2_ERRS.INVLD_PRM})
         else:
             if self.debug:
-                print("RPC.JRPC_Error", ex)
-                #import usys; usys.print_exception(ex)
-                _r.update({"error": {"code": ex.errno, "message": ex.value, "data": ex.args}}) # type:ignore
+                # not all exception have .value/.errno on cpy
+                _r.update({"error": {"type": type(ex), "args": str(ex.args), }}) # type:ignore
             else:
                 _r["id"] = None
                 _r.update({'id': None, "error": JRPC2_ERRS.INTNL_ERR})
